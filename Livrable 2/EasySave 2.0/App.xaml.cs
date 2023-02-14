d1115c741331afb5921208f0cb447c9f1199b275
Lenya Youri <lenyanjeunyouri@gmail.com>
2023-02-14 09:37:50 +0100

    upgrade interface

--- Livrable 2/EasySave 2.0/App.xaml.cs
+++ Livrable 2/EasySave 2.0/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace EasySave_2._0
+namespace EasySaveApp
 {
     /// <summary>
     /// Interaction logic for App.xaml
