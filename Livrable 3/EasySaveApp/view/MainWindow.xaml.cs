f5305b4f8fdb3ee1b39d9fca916883a34b6228dc
RomScz <romain.sconza@viacesi.fr>
2023-02-21 14:31:44 +0100

    update livrable 3

--- Livrable 3/EasySaveApp/view/MainWindow.xaml.cs
+++ Livrable 3/EasySaveApp/view/MainWindow.xaml.cs
@@ -15,6 +15,12 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using EasySaveApp.model;
 using EasySaveApp.viewmodel;
+using System.Diagnostics;
+using System.Threading;
+using System.Net.Sockets;
+using System.Net;
+using System.Windows.Forms;
+using MessageBox = System.Windows.MessageBox;
 
 namespace EasySaveApp.view
 {
@@ -24,16 +30,35 @@ namespace EasySaveApp.view
     public partial class MainWindow : Window
     {
         private ViewModel viewmodel;
-        private string langue;
+        public string langue = "en";
 
         
         public MainWindow()
         {
             InitializeComponent();
             viewmodel = new ViewModel();
-
             ShowListBox();
-            
+
+
+
+            Process Proc_EnCours = Process.GetCurrentProcess(); //Obtains the current process of the application
+
+            Process[] Les_Proc = Process.GetProcesses(); //Collection of currently launched processes
+
+            foreach (Process Processus in Les_Proc)
+                if (Proc_EnCours.Id != Processus.Id) ////If the IDs are different but of the same name
+                {
+                    if (Proc_EnCours.ProcessName == Processus.ProcessName)
+                    {
+                        MessageBox.Show("The EasySeave software is already running.");
+                        this.Close();
+                    }
+                }
+        }
+
+        private void Open_cryptextension(object sender, RoutedEventArgs e)//Function that allows the button to open the file of extensions to be encrypted
+        {
+            System.Diagnostics.Process.Start("notepad.exe", @"..\..\..\Ressources\CryptExtension.json");
         }
 
         private void Button_Click_fr(object sender, RoutedEventArgs e)//Function to translate the software into French
@@ -47,10 +72,13 @@ namespace EasySaveApp.view
             target_name.Content = "Chemin de destination:";
             mirror_name.Content = "Chemin de la sauvegarde complete:";
             text_addsave.Text = "AJOUTER UNE SAUVEGARDE";
+            textcrypt.Text = "Parametre des extensions de \n" +
+                "Cryptographie";
             text_startbackup.Text = "DEMARER LA SAUVEGARDE";
+            textbacklist.Text = "LOGICIEL A BANNIR";
             francaistext.Text = "FRANCAIS";
             englishtext.Text = "ENGLISH";
-
+            textextensionprio.Text = "EXTENSION PRIORITAIRE";
 
         }
 
@@ -65,9 +93,12 @@ namespace EasySaveApp.view
             target_name.Content = "Target folder path: ";
             mirror_name.Content = "Mirror folder backup";
             text_addsave.Text = "ADD SAVE";
+            textcrypt.Text = "PARAMETER OF CRYPTAGE";
             text_startbackup.Text = "START THE BACKUP";
+            textbacklist.Text = "BLACKLIST SOFTWARE";
             francaistext.Text = "FRENCH";
             englishtext.Text = "ENGLISH";
+            textextensionprio.Text = "PRIORITY EXTENSION";
 
         }
 
@@ -146,54 +177,88 @@ namespace EasySaveApp.view
 
                     ShowListBox();//Function to update the list.
                 }
-
             }
-           
         }
 
-      
+        private void source_directory_Click(object sender, RoutedEventArgs e)//Function to retrieve the path to the source folder
+        {
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog(); //Declaration of the method to open the window to choose the folder path.
+            dialog.IsFolderPicker = true;
+
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                SoureDir.Text = dialog.FileName; //Displays the path in the window text.
+            }
+        }
+
+        private void target_directory_Click(object sender, RoutedEventArgs e)//Function to retrieve the path to the destination folder
+        {
+
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog(); //Declaration of the method to open the window to choose the folder path.
+            dialog.IsFolderPicker = true;
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                TargetDir.Text = dialog.FileName; //Displays the path in the window text.
+            }
+        }
+
+        private void mirror_directory_Click(object sender, RoutedEventArgs e)//Function to retrieve the folder path of the mirror backup.
+        {
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog(); //Declaration of the method to open the window to choose the folder path.
+            dialog.IsFolderPicker = true;
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                MirrorDir.Text = dialog.FileName; //Displays the path in the window text.
+            }
+        }
+
+        private void ShowListBox() //Function that displays the names of the backups in the list.
+        {
+
+            Save_work.Items.Clear();
+
+            List<string> names = viewmodel.ListBackup();
+            foreach(string name in names)//Loop that allows you to manage the names in the list.
+            {
+                Save_work.Items.Add(name); //Function that allows you to insert the names of the backups in the list.
+            }
+        }
+
+        private void mirror_button_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_exit(object sender, RoutedEventArgs e)//Function of the button to close the software
+        {
+            Process.GetCurrentProcess().Kill();
+        }
+
+        private void GridMenu_MouseDown(object sender, RoutedEventArgs e)//Function that allows you to move the software window.
+        {
+            DragMove();//Function that allows movement
+        }
 
         private void button_startsave_Click(object sender, RoutedEventArgs e)//Function that launches the backup
         {
             string saveName = "";
 
-            if(Save_work.SelectedItem != null) //Condition that allows to check if the user has selected a backup.
+            if (Save_work.SelectedItem != null) //Condition that allows to check if the user has selected a backup.
             {
                 foreach(string item in Save_work.SelectedItems)//Loop that allows you to select multiple saves
                 {
-                    saveName = item.ToString();
-                    viewmodel.LoadBackup(saveName);
+                    try
+                    {
+                        saveName = item.ToString();
+                        //viewmodel.LoadBackup(saveName, langue);
+                        new Thread(() => viewmodel.LoadBackup(saveName, langue)).Start();
 
-                    if(viewmodel.blackilist_stop == false) //Check for message display if blacklisted software was detected.
-                    {
-                        if (langue == "fr")
-                        {
-                            result.Text = "ECHEC DE SAUVEGARDE ❎\n" +
-                                "ERREUR N°1 : LOGICIEL BLACKLIST \n" +
-                                "EN COURS D'EXECUTION";
-                        }
-                        else
-                        {
-                            result.Text = "BACKUP FAILURE ❎\n" +
-                                "ERROR N°1 : BLACKLIST SOFTWARE\n" +
-                                "IN PROGRESS";
-                        }
                     }
-                    else
+                    catch
                     {
-                        if (langue == "fr")
-                        {
-                            result.Text = "SAUVEGARDE REUSSIE ✅";
-                        }
-                        else
-                        {
-                            result.Text = "SUCCESSFUL BACKUP ✅";
-                        }
+                        
                     }
-                   
                 }
-
-
             }
         }
 
@@ -202,6 +267,34 @@ namespace EasySaveApp.view
             System.Diagnostics.Process.Start("notepad.exe", @"..\..\..\Ressources\BlackList.json");
         }
 
- 
+        private void Open_extensionprio(object sender, RoutedEventArgs e)//Function allowing the button to open the file for priority extensions
+        {
+            System.Diagnostics.Process.Start("notepad.exe", @"..\..\..\Ressources\Priority.json");
+        }
+
+        private void Button_minimize(object sender, RoutedEventArgs e)//Function to reduce the window.
+        {
+            WindowState = (WindowState)FormWindowState.Minimized;
+        }
+
+        private void Button_Play_click(object sender, RoutedEventArgs e)
+        {
+            viewmodel.PlayButton_click();
+        }
+
+        private void Button_Pause_click(object sender, RoutedEventArgs e)
+        {
+            viewmodel.PauseButton_click();
+        }
+
+        private void Button_Stop_click(object sender, RoutedEventArgs e)
+        {
+            viewmodel.StopButton_click();
+        }
+
+        private void Save_work_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
     }
 }
